87bdcfd7a3d0074774526f24fa9f2ad363619916
rayhan889 <raydev1604@gmail.com>
2023-08-07 14:32:26 +0700

    Customer & Room class initiate

--- index.cpp
+++ index.cpp
@@ -5,3 +5,34 @@
 #define max 100
 using namespace std;
 
+class Customer
+{
+    char name[75];
+    char address[100];
+    char phone[12];
+    char checkIn[20];
+    char checkOut[20];
+    float payment;
+    int bookingId;
+};
+
+class Room
+{
+    char roomType;
+    char size;
+    bool isAc;
+    int roomNumber;
+    bool isAvailable;
+
+    class Customer customer;
+    class Room addRoom(int);
+    void searchRoom(int);
+    void deleteRoom(int);
+    void displayRoom(Room);    
+};
+
+
+int main()
+{
+    
+}

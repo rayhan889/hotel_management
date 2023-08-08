170384820ebcded24847504cf2474017bd4b16e7
rayhan889 <raydev1604@gmail.com>
2023-08-08 10:26:54 +0700

    Main func and public access to few classes

--- index.cpp
+++ index.cpp
@@ -7,36 +7,37 @@ using namespace std;
 
 class Customer
 {
-    char name[75];
-    char address[100];
-    char phone[12];
-    char checkIn[20];
-    char checkOut[20];
-    float payment;
-    int bookingId;
+    public:
+        char name[75];
+        char address[100];
+        char phone[12];
+        char checkIn[20];
+        char checkOut[20];
+        float payment;
+        int bookingId;
 };
 
 class Room
 {
-    char roomType;
-    char size;
-    bool isAc;
-    int roomNumber;
-    // 0 = available / 1 = unavailable;
-    int status;
+    public:
+        char roomType;
+        char size;
+        bool isAc;
+        int roomNumber;
+        // 0 = available / 1 = unavailable;
+        int status;
 
-    class Customer customer;
-    class Room addRoom(int);
-    void searchRoom(int);
-    void deleteRoom(int);
-    void displayRoom(Room);    
+        class Customer customer;
+        class Room addRoom(int);
+        void searchRoom(int);
+        void deleteRoom(int);
+        void displayRoom(Room);    
 };
 
 Room Room::addRoom(int rnum)
 {
     class Room room;
-    cout << "Room number: \n";
-    cin >> rnum;
+    room.roomNumber = rnum;
     cout << "\n Room type: ";
     cin >> room.roomType;
     cout << "\n Room size: ";
@@ -45,11 +46,22 @@ Room Room::addRoom(int rnum)
     cin >> room.isAc;
     room.status = 0;
 
-    cout << "Room number: " << rnum << "Successfully added!";
+    cout << "\n Room number: " << rnum << "Successfully added!";
     return room;
 }
 
 int main()
 {
+    system("cls");
+    cout<<"######## Hotel Management #########\n";
+    cout<<"\n1. Manage Rooms";
+    cout<<"\n2. Check-In Room";
+    cout<<"\n3. Available Rooms";
+    cout<<"\n4. Search Customer";
+    cout<<"\n5. Check-Out Room";
+    cout<<"\n6. Guest Summary Report";
+    cout<<"\n7. Exit";
+    cout<<"\n\nEnter Option: ";
 
-}
+    return 0;
+};
